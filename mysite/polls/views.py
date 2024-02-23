1aa05e45a0f071f195d42f4c21f6f79f2c51625e
skorpioness <skorpion25xd@gmail.com>
2024-02-23 08:39:39 +0100

    innit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 def counter(request):
-    text_file = open("./templates/countej.html", "r")
+    text_file = open("./templates/counter.html", "r")
     page = text_file.read()
     text_file.close()
     return HttpResponse(page)
 def basic(request):
-    text_file = open("./templates/countej.html", "r")
+    text_file = open("./templates/counter.html", "r")
     page = text_file.read()
     text_file.close()
