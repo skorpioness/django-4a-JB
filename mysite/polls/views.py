42d88444d6f7bab8b3fbd6f49297d89eae7b26ca
skorpioness <skorpion25xd@gmail.com>
2024-02-23 08:47:31 +0100

    innit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,6 +8,6 @@ def counter(request):
     text_file.close()
     return HttpResponse(page)
 def basic(request):
-    text_file = open("./templates/counter.html", "r")
+    text_file = open("./templates/basic.html", "r")
     page = text_file.read()
     text_file.close()
