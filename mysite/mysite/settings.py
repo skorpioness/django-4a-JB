cf7036b1d776d6ad2bf9407478ee5ef6877be8a2
skorpioness <skorpion25xd@gmail.com>
2024-03-01 08:05:44 +0100

    i

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
-    'polls',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
