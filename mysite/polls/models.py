fe033ba0d435bd67a8a15f9080c271bb3a3c1657
skorpioness <skorpion25xd@gmail.com>
2024-03-01 08:40:17 +0100

    innit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -17,9 +20,5 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-    import datetime
-
-from django.db import models
-from django.utils import timezone
 
 
