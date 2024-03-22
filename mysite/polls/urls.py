b5e3870f44d24f0558d793620b006d11d8116c78
skorpioness <skorpion25xd@gmail.com>
2024-03-22 08:44:31 +0100

    innit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 from .models import Question
 
 APP_NAME = "polls"
-
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("counter/", views.counter, name="counter"),
     path("basic/", views.basic, name="basic"),
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
