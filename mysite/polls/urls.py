5fa7ee4f9744174903b28743013d6db91ffdb57e
skorpioness <skorpion25xd@gmail.com>
2024-03-22 08:18:08 +0100

    innit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,8 +7,13 @@ urlpatterns = [
     path("counter/", views.counter, name="counter"),
     path("basic/", views.basic, name="basic"),
     path("", views.index, name="index"),
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
 
